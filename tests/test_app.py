cc8a49aca7605422de3751ba2cacc9f3a6f41f5e
Sehat1137 <Sehat1137@users.noreply.github.com>
2025-07-01 02:46:27 +0300

    Init

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from httpx import ASGITransport, AsyncClient
+from api.main import app
+
+
+@pytest.mark.asyncio
+async def test_read_root():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        response = await client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
