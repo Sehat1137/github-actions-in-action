aa1ae7ac4e4c52f2f494707d64d5d1d2de8aeec2
Sehat1137 <Sehat1137@users.noreply.github.com>
2025-08-19 17:14:12 +0300

    feat: add router description

--- api/main.py
+++ api/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI(title="GitHub Actions in Action")
 
 
-@app.get("/")
+@app.get("/", description="Root endpoint")
 async def root():
     return {"Hello": "World"}
